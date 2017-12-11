3f859422d1cf549b1c20e23c59db535e45929862
nattynnat <natty13944@gmail.com>
2017-12-11 19:56:35 +0700

    ???

--- src/Main.java
+++ src/Main.java
@@ -12,30 +12,7 @@ public class Main extends Application {
 		stage.setResizable(false);
 		stage.sizeToScene();	
 		SceneManager sm = new SceneManager(stage);
-<<<<<<< HEAD
-		sm.gotoMainMenu();
-		AnimationTimer animation = new AnimationTimer() {
-			public void handle(long now) {
-			}
-		};
-		animation.start();
-		
-		
-||||||| merged common ancestors
-		sm.gotoMainMenu();
-		AnimationTimer animation = new AnimationTimer() {
-			public void handle(long now) {
-//				gs.paintScreen();
-//				gameLogic.logicUpdate();
-//				RenderableHolder.getInstance().update();
-			}
-		};
-		animation.start();
-		
-		
-=======
 		SceneManager.gotoMainMenu();		
->>>>>>> 961e98fbf3e2073150f43745be1d2cfd869d0245
 	}
 
 	public void stop(Stage stage) {
